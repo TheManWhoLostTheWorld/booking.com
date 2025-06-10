4af7ebe0afe888a585c3b272de37491ed3f9c4dd
I_Call_You_Back <denis.z.germany@gmail.com>
2025-06-10 22:37:34 +0300

    finish working on scroll to element helper

--- base/base_test.py
+++ base/base_test.py
@@ -2,6 +2,7 @@ from pages.stays_page import StaysPage
 from pages.car_rental_page import CarRentalsPage
 from helpers.calendar_helper import CalendarHelper
 from helpers.people_helper import PeopleHelper
+from helpers.ui_helper import UiHelper
 
 class BaseTest:
 
@@ -10,3 +11,4 @@ class BaseTest:
        self.car_rental_page = CarRentalsPage(self.driver)
        self.calendar = CalendarHelper(self.driver)
        self.people = PeopleHelper(self.driver)
+       self.ui = UiHelper(self.driver)
--- helpers/ui_helper.py
+++ helpers/ui_helper.py
@@ -1,4 +1,19 @@
+from base.base_page import BasePage
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
+class UiHelper(BasePage):
 
-class UiHelper:
-    ...
+    def __init__(self, driver):
+        self.driver: WebDriver = driver
+        self.wait = WebDriverWait(self.driver, 15, 1)
+        self.action = ActionChains(self.driver)
+
+    # def find(self, locator):
+    #     return self.wait.until(EC.visibility_of_element_located(locator))
+
+    def scroll_to(self, locator):
+        self.action.scroll_to_element(self.wait.until(EC.visibility_of_element_located(locator)))
+        self.driver.execute_script("""window.scrollTo({top: window.scrollY + 500,});""")
