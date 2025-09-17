e85a4b7dc5ba31e31e2830a8ce2f62c58ce1ca64
I_Call_You_Back <denis.z.germany@gmail.com>
2025-09-17 21:41:10 +0300

    add test marks

--- tests/test_stays_page.py
+++ tests/test_stays_page.py
@@ -1,12 +1,16 @@
 from base.base_test import BaseTest
+import pytest
 import time
 
 class TestStays(BaseTest):
 
+    @pytest.mark.regression
     def test_open_stays_page(self):
         self.stays_page.open()
         self.stays_page.click_stays()
 
+    @pytest.mark.smoke
+    @pytest.mark.regression
     def test_search_city(self):
         self.stays_page.open()
         self.stays_page.cancel_registration_window()
@@ -21,6 +25,8 @@ class TestStays(BaseTest):
         self.stays_page.cancel_registration_window()
         self.stays_page.proof_location("Hamburg")
 
+    @pytest.mark.smoke
+    @pytest.mark.regression
     def test_dropdown_click(self):
         self.stays_page.open()
         self.stays_page.cancel_registration_window()
@@ -37,6 +43,8 @@ class TestStays(BaseTest):
         self.stays_page.cancel_registration_window()
         self.stays_page.proof_location("Beirut")
 
+    @pytest.mark.smoke
+    @pytest.mark.regression
     def test_search_specific_hotel(self):
         self.stays_page.open()
         self.stays_page.cancel_registration_window()
@@ -52,6 +60,8 @@ class TestStays(BaseTest):
         self.stays_page.cancel_registration_window()
         self.stays_page.proof_hotel("Radisson Blu")
 
+    @pytest.mark.smoke
+    @pytest.mark.regression
     def test_search_country(self):
         self.stays_page.open()
         self.stays_page.cancel_registration_window()
@@ -67,18 +77,20 @@ class TestStays(BaseTest):
         self.stays_page.cancel_registration_window()
         self.stays_page.proof_location("Germany")
 
-    # def test_search_date_in_the_past(self):
-    #     self.stays_page.open()
-    #     self.stays_page.accept_cookies()
-    #     self.stays_page.add_destination("Paris")
-    #     self.stays_page.accept_cookies()
-    #     self.calendar.open()
-    #     self.calendar.set_date(8, 6, 2025)
-    #     self.calendar.set_date(15, 6, 2025)
-    #     self.people.set_people(2, 3)
-    #     self.people.ages(0, 8, 17)
-    #     self.stays_page.click_search()
-    #     self.stays_page.proof_location("Paris")
+    @pytest.mark.negative
+    @pytest.mark.regression
+    def test_search_date_in_the_past(self):
+        self.stays_page.open()
+        self.stays_page.accept_cookies()
+        self.stays_page.add_destination("Paris")
+        self.stays_page.accept_cookies()
+        self.calendar.open()
+        self.calendar.set_date(8, 6, 2025)
+        self.calendar.set_date(15, 6, 2025)
+        self.people.set_people(2, 3)
+        self.people.ages(0, 8, 17)
+        self.stays_page.click_search()
+        self.stays_page.proof_location("Paris")
 
     # def test_scroll_to_element(self):
     #     self.stays_page.open()
